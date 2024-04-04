a858eb6f90b88386dc63b604db5106d7640a99eb
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-04-04 16:51:05 +0200

    add: task of palindromo exercise without function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+//* richiesta input utente
 const userWord = prompt("Ciao, dimmi una parola e ti dirò se è palindroma");
+
+//*ciclo for
+let result1 = "";
+for (let i = 0; i < userWord.length; i++) {
+    const curLetter = userWord[i];
+    result1 +=userWord[i]
+}
+console.log(result1);
+
+//*ciclo for al contrario
+let result2 = "";
+for(let j = userWord.length -1; j >= 0; j--) {
+    const curLetter2 = userWord[j];
+    result2 +=userWord[j]
+}
+console.log(result2);
+
+//*variabile booleana
+let palindromo = false
+
+//*condizione
+if (result1 === result2) {
+    console.log("La parola è palindroma");
+} else {
+    console.log("La parola non è palindroma");
+}
